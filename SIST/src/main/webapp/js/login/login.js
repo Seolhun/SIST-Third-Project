35a4d01c6f1260391904c4bdfb1d47f222d7c143
Seolhun <shun10114@github.com>
2016-05-17 19:50:52 +0900

    "mouse over js"

--- SIST/src/main/webapp/js/login/login.js
+++ SIST/src/main/webapp/js/login/login.js
@@ -4,109 +4,129 @@
 
 /*구글 로그인*/
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
+	var profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail());
 }
 
 function onSuccess(googleUser) {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+	console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
 }
 function onFailure(error) {
-    console.log(error);
+	console.log(error);
 }
 function renderButton() {
-    gapi.signin2.render('my-signin2', {
-        'scope': 'profile email',
-        'width': 300,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSuccess,
-        'onfailure': onFailure
-    });
+	gapi.signin2.render('my-signin2', {
+		'scope' : 'profile email',
+		'width' : 300,
+		'height' : 50,
+		'longtitle' : true,
+		'theme' : 'dark',
+		'onsuccess' : onSuccess,
+		'onfailure' : onFailure
+	});
 }
 
 /*구글 로그아웃*/
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function() {
+		console.log('User signed out.');
+	});
 }
 
+/*function cambiar_login() {
+ document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";
+ document.querySelector('.cont_form_login').style.display = "block";
+ document.querySelector('.cont_form_sign_up').style.opacity = "0";
+ document.querySelector('.cont_form_sign_up2').style.opacity = "0";
 
+ setTimeout(function() {
+ document.querySelector('.cont_form_login').style.opacity = "1";
+ }, 400);
 
-function cambiar_login() {
-    document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";
-    document.querySelector('.cont_form_login').style.display = "block";
-    document.querySelector('.cont_form_sign_up').style.opacity = "0";
-    document.querySelector('.cont_form_sign_up2').style.opacity = "0";
+ setTimeout(function() {
+ document.querySelector('.cont_form_sign_up').style.display = "none";
+ }, 200);
+ }
 
-    setTimeout(function() {
-        document.querySelector('.cont_form_login').style.opacity = "1";
-    }, 400);
+ function cambiar_sign_up(at) {
+ document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
+ document.querySelector('.cont_form_sign_up').style.display = "block";
+ document.querySelector('.cont_form_login').style.opacity = "0";
 
-    setTimeout(function() {
-        document.querySelector('.cont_form_sign_up').style.display = "none";
-    }, 200);
-}
+ setTimeout(function() {
+ document.querySelector('.cont_form_sign_up').style.opacity = "1";
+ }, 100);
 
-function cambiar_sign_up(at) {
-    document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
-    document.querySelector('.cont_form_sign_up').style.display = "block";
-    document.querySelector('.cont_form_login').style.opacity = "0";
+ setTimeout(function() {
+ document.querySelector('.cont_form_login').style.display = "none";
+ }, 400);
+ }
 
-    setTimeout(function() {
-        document.querySelector('.cont_form_sign_up').style.opacity = "1";
-    }, 100);
+ function cambiar_sign_up2(at) {
+ document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
+ document.querySelector('.cont_form_sign_up2').style.display = "block";
+ document.querySelector('.cont_form_login').style.opacity = "0";
 
-    setTimeout(function() {
-        document.querySelector('.cont_form_login').style.display = "none";
-    }, 400);
+ setTimeout(function() {
+ document.querySelector('.cont_form_sign_up2').style.opacity = "1";
+ }, 100);
 
-}
-function cambiar_sign_up2(at) {
-    document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
-    document.querySelector('.cont_form_sign_up2').style.display = "block";
-    document.querySelector('.cont_form_login').style.opacity = "0";
+ setTimeout(function() {
+ document.querySelector('.cont_form_login').style.display = "none";
+ }, 400);
+ */
 
-    setTimeout(function() {
-        document.querySelector('.cont_form_sign_up2').style.opacity = "1";
-    }, 100);
+/!* 마우스 오버!!*!/
+if (document.querySelector) {
+	var loginDiv = document.querySelector('#loginDiv')
+	var signDiv = document.querySelector('#signDiv')
 
-    setTimeout(function() {
-        document.querySelector('.cont_form_login').style.display = "none";
-    }, 400);
+	loginDiv.onmouseover = function() {
+		document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";
+		document.querySelector('.cont_form_login').style.display = "block";
+		document.querySelector('.cont_form_sign_up').style.opacity = "0";
+		document.querySelector('.cont_form_sign_up2').style.opacity = "0";
 
-/*    /!* 마우스 오버!!*!/
-    if (document.querySelector){
-        var loginDiv=document.querySelector('#loginDiv')
-        var signDiv=document.querySelector('#signDiv')
+		setTimeout(function() {
+			document.querySelector('.cont_form_login').style.opacity = "1";
+		}, 400);
+		setTimeout(
+				function() {
+					document.querySelector('.cont_form_sign_up').style.display = "none";
+				}, 200);
+	}
 
-        loginDiv.onmouseover=function(){
-            this.querySelector('#loginDiv').style.background="yellow"
-        }
-        signDiv.onmouseout=function(){
-            this.querySelector('#signDiv').style.background="silver"
-        }
-    }*/
+	signDiv.onmouseover = function() {
+		document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
+		document.querySelector('.cont_form_sign_up').style.display = "block";
+		document.querySelector('.cont_form_login').style.opacity = "0";
+
+		setTimeout(function() {
+			document.querySelector('.cont_form_sign_up').style.opacity = "1";
+		}, 100);
+
+		setTimeout(function() {
+			document.querySelector('.cont_form_login').style.display = "none";
+		}, 400);
+	}
+
 }
 
 function ocultar_login_sign_up() {
 
-    document.querySelector('.cont_forms').className = "cont_forms";
-    document.querySelector('.cont_form_sign_up').style.opacity = "0";
-    document.querySelector('.cont_form_sign_up2').style.opacity = "0";
-    document.querySelector('.cont_form_login').style.opacity = "0";
+	document.querySelector('.cont_forms').className = "cont_forms";
+	document.querySelector('.cont_form_sign_up').style.opacity = "0";
+	document.querySelector('.cont_form_sign_up2').style.opacity = "0";
+	document.querySelector('.cont_form_login').style.opacity = "0";
 
-
-    setTimeout(function() {
-        document.querySelector('.cont_form_sign_up').style.display = "none";
-        document.querySelector('.cont_form_sign_up2').style.display = "none";
-        document.querySelector('.cont_form_login').style.display = "none";
-    }, 500);
+	setTimeout(function() {
+		document.querySelector('.cont_form_sign_up').style.display = "none";
+		document.querySelector('.cont_form_sign_up2').style.display = "none";
+		document.querySelector('.cont_form_login').style.display = "none";
+	}, 500);
 
 }
